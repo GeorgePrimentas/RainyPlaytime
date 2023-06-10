4d5a8c67e2fb87473e964a33f6b1dc86ac4e26fd
George <108883931+GeorgePrimentas@users.noreply.github.com>
2023-06-10 06:01:23 +0100

    Basic Error Handling (Step 8)
    Not slicky but at least if the user inputs a city that the API doesn't have any information about the app will rerun instead of breaking down.

--- src/components/InputCity/InputCity.jsx
+++ src/components/InputCity/InputCity.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const InputCity = ({ setWeatherData }) => {
   const [cityName, setCityName] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleInputChange = (event) => {
     setCityName(event.target.value);
@@ -12,8 +13,15 @@ const InputCity = ({ setWeatherData }) => {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&cnt=8&units=metric&appid=1d162b7fec3f97b54fcd4bc4510b95b2`)
       .then((response) => response.json())
       .then((jsonData) => {
+        console.log(jsonData.message);
         setWeatherData(jsonData); // Update the weatherData state in the App component
-      })
+        const message = jsonData.message; // The message that json sends
+        setMessage(message);
+      
+        if (message === "city not found") {
+      window.location.reload();
+      }
+      }) 
       .catch((error) => {
         console.log('Error fetching data:', error);
       });
@@ -23,8 +31,9 @@ const InputCity = ({ setWeatherData }) => {
     <div className="topSection">
       <input className="inputField" placeholder="Type in a city name" type="text" onChange={handleInputChange} />
       <button className="button" onClick={handleButtonClick}>find weather</button>
-      {/* <span>F</span><input type="radio" name="scale" />
-       <input type="radio" name = "scale" checked /><span>C</span> */}
+      {/* Radio buttons for switching scales (Celcius / Farenheit)
+      <span>F</span><input type="radio" name="scale" />
+      <input type="radio" name = "scale" checked /><span>C</span> */}
     </div>
   );
 };
